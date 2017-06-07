3a5c7fbf47e3e292857298895e4e9c4af94794f7
Tsung Chun Hsu <osoo20906@gmail.com>
2017-06-07 20:40:33 +0000

    blog

---
+++ author/form.py
@@ -0,0 +1,28 @@
+from flask_wtf import FlaskForm
+from wtforms import validators, StringField, PasswordField
+from wtforms.fields.html5 import EmailField
+
+class RegisterForm(FlaskForm):
+    fullname = StringField('Full Name', [validators.Required()])
+    email = EmailField('Email', [validators.Required()])
+    username = StringField('Username', [
+        validators.Required(),
+        validators.Length(min=4, max=25)
+        ])
+    password = PasswordField('New Password', [
+        validators.Required(),
+        validators.EqualTo('confirm', message='Passwords must match'),
+        validators.Length(min=4, max=80)
+        ])
+    confirm = PasswordField('Repeat Password')
+    
+    
+class LoginForm(FlaskForm):
+    username = StringField('Username', [
+        validators.Required(),
+        validators.Length(min=4, max=25)
+        ])
+    password = PasswordField('Password', [
+        validators.Required(),
+        validators.Length(min=4, max=25)
+        ])
