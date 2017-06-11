a2dac1f6e687047df9f39b579915769e3d12955b
Tsung Chun Hsu <tsungchh@gmail.com>
2017-06-11 19:17:45 +0000

    docker change

--- settings.py
+++ settings.py
@@ -2,12 +2,12 @@ import os
 SECRET_KEY = 'you-will-never-guess'
 DEBUG = True
 
-DB_USERNAME = 'tsungchh'
-DB_PASSWORD = ''
+DB_USERNAME = 'root'
+DB_PASSWORD = 'test'
 BLOG_DATABASE_NAME = 'blog'
-DB_HOST = os.getenv('IP', '0.0.0.0')
+DB_HOST = 'mysql:3306'
 DB_URI = "mysql+pymysql://%s:%s@%s/%s" %(DB_USERNAME, DB_PASSWORD, DB_HOST, BLOG_DATABASE_NAME)
 SQLALCHEMY_DATABASE_URI = DB_URI
 SQLALCHEMY_TRACK_MODIFICATIONS = True
-UPLOADED_IMAGES_DEST = '/home/ubuntu/workspace/flask_blog/static/images'
+UPLOADED_IMAGES_DEST = '/opt/flask_blog/static/images'
 UPLOADED_IMAGES_URL = '/static/images/'
